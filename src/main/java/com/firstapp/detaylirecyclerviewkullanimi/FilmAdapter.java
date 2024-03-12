34f2a33895fa6ba7c13bc91f137fe262fa3ed856
Gorur56 <sevim.gorur01@gmail.com>
2024-03-12 20:43:04 +0300

    update main activity

--- src/main/java/com/firstapp/detaylirecyclerviewkullanimi/FilmAdapter.java
+++ src/main/java/com/firstapp/detaylirecyclerviewkullanimi/FilmAdapter.java
@@ -1,12 +1,20 @@
 package com.firstapp.detaylirecyclerviewkullanimi;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class FilmAdapter {
+public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.CardViewTasarimNesneleriniTutucu>{
     private Context mContext;
     private List<Filmler> filmlerList;
 
@@ -14,8 +22,52 @@ public class FilmAdapter {
         this.mContext = mContext;
         this.filmlerList = filmlerList;
     }
-
     public class CardViewTasarimNesneleriniTutucu extends RecyclerView.ViewHolder {
-        
+        public ImageView imageFilmResim;
+        public TextView textViewFilmBaslik;
+        public TextView textViewFilmFiyat;
+        public Button buttonSepeteEkle;
+
+        public CardViewTasarimNesneleriniTutucu(@NonNull View itemView) {
+            super(itemView);
+            imageFilmResim = itemView.findViewById(R.id.imageViewFilmResim);
+
+            textViewFilmBaslik = itemView.findViewById(R.id.textViewFilmBaslik);
+            textViewFilmFiyat = itemView.findViewById(R.id.textViewFilmFiyat);
+
+            buttonSepeteEkle = itemView.findViewById(R.id.buttonSepeteEkle);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CardViewTasarimNesneleriniTutucu onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_film_tasarim, parent,false);
+        //CardViewTasarimNesneleriniTutucu sınıfından bir nesne oluşturmamız gerekiyor.
+        return new CardViewTasarimNesneleriniTutucu(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewTasarimNesneleriniTutucu holder, int position) {
+        final Filmler film = filmlerList.get(position);
+
+        holder.textViewFilmBaslik.setText(film.getFilm_id());
+        holder.textViewFilmFiyat.setText(film.getFilm_fiyat()+" TL");
+
+        holder.imageFilmResim.setImageResource(R.drawable.interstellar);
+
+
+        holder.buttonSepeteEkle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext,film.getFilm_ad()+" sepete Eklendi.",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return filmlerList.size();
     }
 }
--- src/main/java/com/firstapp/detaylirecyclerviewkullanimi/MainActivity.java
+++ src/main/java/com/firstapp/detaylirecyclerviewkullanimi/MainActivity.java
@@ -7,9 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView rv;
+    private ArrayList<Filmler> filmlerArrayList;
+    private FilmAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        rv = findViewById(R.id.rv);
+        rv.setHasFixedSize(true);
+
+        //CArd oluşumu tek sırada 2 tane olacak
+        rv.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        Filmler f1 = new Filmler(1, "Django", "django",89.99);
+        Filmler f2 = new Filmler(2, "Inception", "inception",99.99);
+        Filmler f3 = new Filmler(3, "Interstellar", "interstellar",119.90);
+        Filmler f4 = new Filmler(4, "The Hateful Eight", "thehatefuleight",79.99);
+        Filmler f5 = new Filmler(5, "The Pianist", "thepianist",89.99);
+        Filmler f6 = new Filmler(1, "Bir Zamanlar Anadoluda", "birzamanlaranadoluda",69.99);
+
+
+        //array list oluşturma film nesnelerini yerleştirmek için
+
+        filmlerArrayList = new ArrayList<>();
+
+        filmlerArrayList.add(f1);
+        filmlerArrayList.add(f2);
+        filmlerArrayList.add(f3);
+        filmlerArrayList.add(f4);
+        filmlerArrayList.add(f5);
+        filmlerArrayList.add(f6);
+
     }
 }
